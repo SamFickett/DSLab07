fe0ffacf94a1289e921a0e6dac293e658f596d96
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2023-11-06 14:19:09 -0500

    Initial commit

---
+++ myFoodPopularity.java
@@ -0,0 +1,15 @@
+package Lab7;
+/*
+ * @author Jere Perisic
+ * @version November 6, 2023
+ * myFoodPopularity class method getFoodPopularity, takes treemap as an input with country's name as key.
+ *
+ */
+import java.util.*;
+public class myFoodPopularity implements IFoodPopularity{
+        @Override
+        public List<String> getFoodPopularity(TreeMap<String, TreeSet<String>> countryFoodSet, List<String> countries1, List<String> countries2) {
+            return countries1;
+        }
+
+}
