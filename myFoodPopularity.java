c4fda6c1f764e904ca75edf4563ef05dc97a60ee
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2023-11-06 14:33:05 -0500

    Made arrayList(result), and TreeSet(uniqueFoods)

--- myFoodPopularity.java
+++ myFoodPopularity.java
@@ -1,15 +1,30 @@
 package Lab7;
+import java.util.*;
 /*
  * @author Jere Perisic
  * @version November 6, 2023
  * myFoodPopularity class method getFoodPopularity, takes treemap as an input with country's name as key.
- *
  */
-import java.util.*;
-public class myFoodPopularity implements IFoodPopularity{
-        @Override
-        public List<String> getFoodPopularity(TreeMap<String, TreeSet<String>> countryFoodSet, List<String> countries1, List<String> countries2) {
-            return countries1;
+public class myFoodPopularity implements IFoodPopularity {
+    @Override
+    public List<String> getFoodPopularity(TreeMap<String, TreeSet<String>> countryFoodSet, List<String> countries1, List<String> countries2) {
+        List<String> result = new ArrayList<>();
+
+        if (countries1.isEmpty() || countries2.isEmpty()) {
+            return result;
         }
 
+        int popularMajority1 = countries1.size() / 2 + 1;
+        int popularMajority2 = countries2.size() / 2 + 1;
+
+        TreeSet<String> uniqueFoods = new TreeSet<>();
+
+        for (Set<String> foodSet : countryFoodSet.values()) {
+            uniqueFoods.addAll(foodSet);
+        }
+
+
+
+        return result;
+    }
 }
